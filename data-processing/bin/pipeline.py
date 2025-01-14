58429d344736f10cd4608fdb8bbe7ee6fdbe72e2
Giorgio C <ceo@acaciatech.et>
2025-01-14 10:47:59 +0300

    Add pipeline entry point
    Main function of the Pipeline module will be the entry point

---
+++ data-processing/bin/pipeline.py
@@ -0,0 +1,26 @@
+
+from ..src.data_pipeline.miner import extract_data
+from ..src.data_pipeline.cleaner import clean_data
+from ..src.data_pipeline.transformer import transform_data
+from ..src.data_pipeline.exporter import export_data
+
+def main():
+
+    # Step 1: Mining
+    raw_data = extract_data("data/input/pdfs/")
+    print("Step 1: Data extracted.")
+
+    # Step 2: Cleaning
+    cleaned_data = clean_data(raw_data)
+    print("Step 2: Data cleaned.")
+
+    # Step 3: Transformation
+    transformed_data = transform_data(cleaned_data)
+    print("Step 3: Data transformed.")
+
+    # Step 4: Exporting
+    export_data(transformed_data, "data/output/processed_data.csv")
+    print("Step 4: Data exported.")
+
+if __name__ == "__main__":
+    main()
