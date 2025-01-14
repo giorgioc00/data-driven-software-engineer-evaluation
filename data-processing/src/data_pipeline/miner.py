5a8b150eef8c0b44d7eaa5a3a056e271b5d966a6
Giorgio C <ceo@acaciatech.et>
2025-01-14 15:24:53 +0300

    Add 'load_pdf' function working with PyPDF2 lib

--- data-processing/src/data_pipeline/miner.py
+++ data-processing/src/data_pipeline/miner.py
@@ -1,4 +1,33 @@
 from ..utils.finder import collect_files
 import logging
+import PyPDF2
+
 
 logger = logging.getLogger(__name__)
+
+def miner():
+    files = collect_files()
+    logging.debug(f"Number of files found {len(files)}")
+    load_pdf(files[0])
+
+
+def load_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        print(f"Number of pages: {len(reader.pages) }")
+
+        meta = reader.metadata
+
+        # All of the following could be None!
+        print(meta.author)
+        print(meta.creator)
+        print(meta.producer)
+        print(meta.subject)
+        print(meta.title)
+
+        page = reader.pages[0]
+        print(page.extract_text())
+
+
+if __name__ == "__main__":
+    miner()
