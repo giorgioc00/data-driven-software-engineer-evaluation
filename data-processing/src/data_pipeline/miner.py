dcacc3aa7bb07aa23d24f6ef2e9a9cf9aca73806
Giorgio C <ceo@acaciatech.et>
2025-01-14 16:22:14 +0300

    Add new text reading logic in the read_pdf_data function

--- data-processing/src/data_pipeline/miner.py
+++ data-processing/src/data_pipeline/miner.py
@@ -8,26 +8,32 @@ logger = logging.getLogger(__name__)
 def miner():
     files = collect_files()
     logging.debug(f"Number of files found {len(files)}")
-    load_pdf(files[0])
+
+    logging.debug(read_pdf_data(files[0]))
 
 
-def load_pdf(file_path):
+def read_pdf_data(file_path):
     with open(file_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
-        print(f"Number of pages: {len(reader.pages) }")
+        logging.debug(f"Number of pages: {len(reader.pages)}")
 
+        # Extract metadata
         meta = reader.metadata
 
-        # All of the following could be None!
-        print(meta.author)
-        print(meta.creator)
-        print(meta.producer)
-        print(meta.subject)
-        print(meta.title)
+        # Extract text from all pages
+        pages_text = []
+        for i, page in enumerate(reader.pages):
+            page_text = page.extract_text()
+            pages_text.append(page_text)
 
-        page = reader.pages[0]
-        print(page.extract_text())
+            logging.debug(f"Extracted text from page {i + 1}")
 
+        pdf_data = {
+            "metadata": meta,
+            "pages_text": pages_text,
+        }
+
+        return pdf_data
 
 if __name__ == "__main__":
     miner()
