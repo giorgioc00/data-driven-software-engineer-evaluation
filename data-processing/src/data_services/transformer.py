4bbb8bb49f7eda10c523ac99cf88025799bd862c
Giorgio C <ceo@acaciatech.et>
2025-01-14 19:56:21 +0300

    New pipeline logic
    Still for dev purpose

--- data-processing/src/data_services/transformer.py
+++ data-processing/src/data_services/transformer.py
@@ -3,16 +3,21 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def gen_list_from_text(text: str) -> list:
+    paragraphs = text.split('\n')
+    logging.debug(f'Paragraphs: {len(paragraphs)}')
+    return paragraphs
 
 
 def transformer(pdfs_cleaned_data):
-
     formatted_data = [
         {
             "filename": pdf_data_dict["filename"],
             "metadata": pdf_data_dict["metadata"],
-            "pages_text": pdf_data_dict["pages_text"],
+            "pages_text": [gen_list_from_text(page_text) for page_text in pdf_data_dict["pages_text"]],
         }
         for pdf_data_dict in pdfs_cleaned_data]
 
-    logging.debug(formatted_data)
+    logging.debug(len(formatted_data[0]["pages_text"]))
+
+    # logging.debug(formatted_data)
