e4621d633e01abd402109a2b771929a7fac289d6
Giorgio C <ceo@acaciatech.et>
2025-01-14 12:02:01 +0300

    Update logging config to log in the main.log file

--- data-processing/config/logging_config.py
+++ data-processing/config/logging_config.py
@@ -2,6 +2,9 @@ import logging
 
 def setup_logging():
     logging.basicConfig(
+        filename="var/main.log",
+        encoding="utf-8",
+        filemode="a",
         level=logging.DEBUG,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
