8804bbf3724cc31481035099680218c8975990b2
Giorgio C <ceo@acaciatech.et>
2025-01-14 17:05:35 +0300

    Add function docstring

--- data-processing/src/data_pipeline/miner.py
+++ data-processing/src/data_pipeline/miner.py
@@ -11,6 +11,22 @@ def miner():
 
     logging.debug(read_pdf_data(files[0]))
 
+def get_pdf_data(file_path):
+    """
+    Extracts data from a PDF file, including metadata and text content.
+
+    This function retrieves the metadata of the PDF file (e.g., author, title)
+    and extracts text from all pages of the document. The extracted text is
+    organized page by page.
+
+    :param file_path: str
+        The file path of the PDF to be processed.
+    :return: dict
+        A dictionary containing:
+        - "metadata" (dict): Metadata of the PDF file, such as author, title, etc.
+        - "content" (list): A list of strings where each entry corresponds to
+          the text extracted from a single page.
+    """
 
 def read_pdf_data(file_path):
     with open(file_path, 'rb') as file:
