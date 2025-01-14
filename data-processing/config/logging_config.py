03dbc263f2a64266a04f05c1a492885b13621997
Giorgio C <ceo@acaciatech.et>
2025-01-14 12:15:08 +0300

    Update logging config with two different handlers

--- data-processing/config/logging_config.py
+++ data-processing/config/logging_config.py
@@ -1,11 +1,21 @@
 import logging
 
 def setup_logging():
-    logging.basicConfig(
-        filename="var/main.log",
-        encoding="utf-8",
-        filemode="a",
-        level=logging.DEBUG,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler("var/main.log", encoding="utf-8", mode="a")
+    file_handler.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
     )
+
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
