0a0401779d3acd3e3584cf7a663a18139572e2f7
Giorgio C <ceo@acaciatech.et>
2025-01-15 15:30:25 +0300

    Add new docstring for the transformer module
    To describe his two internal functions

--- data-processing/src/data_services/transformer.py
+++ data-processing/src/data_services/transformer.py
@@ -4,12 +4,24 @@ logger = logging.getLogger(__name__)
 
 
 def gen_list_from_text(text: str) -> list:
+    """
+    This function converts a string into a list of strings splitting the text based on the new line character "\n"
+    :param text:
+    :return: list of strings
+    """
     paragraphs = text.split('\n')
     logging.debug(f'Paragraphs: {len(paragraphs)}')
     return paragraphs
 
 
 def transformer(pdfs_cleaned_data):
+    """
+    This function is the entry point of the Pipeline of the transformer module.
+    Its purpose is to change the data structure and format of data to be ready for the collection.
+
+    :param pdfs_cleaned_data:
+    :return: formatted_data: where inside 'pages_text_list' is formatted as a list of strings
+    """
     formatted_data = [
         {
             "filename": pdf_data_dict["filename"],
